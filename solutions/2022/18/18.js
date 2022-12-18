cb633794d374b62f92e0b2324b882bdb25d6f456
Kamil Pogorzelski <pogorzelski.kamil@gmail.com>
2022-12-18 07:51:03 +0100

    Day 18 solutions

---
+++ solutions/2022/18/18.js
@@ -0,0 +1,64 @@
+import { asc, copy, count, desc, log, map, pairwise, pipe, reduce, sort, split, splitByLine, sum, take, toInt } from "../../../utils.js";
+
+
+const parseInput = pipe(
+  split('\n'),
+  map(split(',')),
+  map(map(toInt))
+)
+
+const first = pipe(
+  parseInput,
+  points => {
+    const pointSet = new Set(points.map(point => point.toString()));
+
+    const transformations = [[0,0,1],[0,0,-1],[0,1,0],[0,-1,0],[1,0,0],[-1,0,0]];
+
+    return points.flatMap(([x,y,z]) => transformations.map(([dx,dy,dz]) => [x+dx,y+dy,z+dz]))
+      .filter(point => !pointSet.has(point.toString())).length
+  },
+);
+
+const translate = ([x,y,z], [dx,dy,dz]) => [x+dx,y+dy,z+dz];
+
+const second = pipe(
+  parseInput,
+  points => {
+    const pointSet = new Set(points.map(point => point.toString()));
+    const transformations = [[0,0,1],[0,0,-1],[0,1,0],[0,-1,0],[1,0,0],[-1,0,0]];
+
+    const xValues = points.map(([x]) => x).sort(asc)
+    const yValues = points.map(([x,y]) => y).sort(asc)
+    const zValues = points.map(([x,y,z]) => z).sort(asc)
+    const minX = xValues[0]-1, maxX = xValues[xValues.length-1] + 1;
+    const minY = yValues[0]-1, maxY = yValues[xValues.length-1] + 1;
+    const minZ = zValues[0]-1, maxZ = zValues[xValues.length-1] + 1;
+
+    let total = 0;
+    const toCheck = [[minX,minY,minZ]];
+    const inCheck = new Set([toCheck[0].toString()])
+
+    while(toCheck.length) {
+      const current = toCheck.shift();
+
+      const adjacent = transformations.map(delta => translate(current, delta));
+
+      const adjacentRocks = adjacent.filter(point => pointSet.has(point.toString()))
+      const adjacentAir = adjacent.filter(point => !pointSet.has(point.toString()))
+
+      total += adjacentRocks.length
+
+      const airToVisit = adjacentAir
+        .filter(point => !inCheck.has(point.toString()))
+        .filter(([x,y,z]) => (x>=minX && x<=maxX)&&(y>=minY && y<=maxY)&&(z>=minZ && z<=maxZ))
+      
+      airToVisit.forEach(point => {
+        toCheck.push(point)
+        inCheck.add(point.toString())
+      })
+    }
+
+    return total;
+  },
+);
+
