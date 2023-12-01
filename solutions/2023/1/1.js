5cbe3bd92a80ef6078db63e300c2b8c00daee725
Kamil Pogorzelski <pogorzelski.kamil@gmail.com>
2023-12-01 21:08:08 +0100

    Day 1 solutions

---
+++ solutions/2023/1/1.js
@@ -0,0 +1,46 @@
+import { asc, copy, count, desc, log, map, filter, negate, join, pairwise, pipe, range, reduce, sort, split, splitByLine, sum, take, toInt, toInts } from "../../../utils.js";
+
+const first = pipe(
+  split('\n'),
+  map(split('')),
+  map(map(toInt)),
+  map(filter(negate(Number.isNaN))),
+  map(arr => [arr[0], arr[arr.length-1]]),
+  map(join('')),
+  map(toInt),
+  sum,
+);
+
+const mapping = Object.entries({
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9'
+})
+
+const second = pipe(
+  split('\n'),
+  map(split('')),
+  map(map((ch, i, arr) => {
+    const spelledDigit = mapping.find(
+      ([k, v]) => arr.join('').slice(i).startsWith(k)
+    );
+
+    if (spelledDigit) {
+      return spelledDigit[1];
+    } else {
+      return ch;
+    }
+  })),
+  map(map(toInt)),
+  map(filter(negate(Number.isNaN))),
+  map(arr => [arr[0], arr[arr.length-1]]),
+  map(join('')),
+  map(toInt),
+  sum,
+);
