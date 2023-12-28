4aeef596ea9e19f86baa7a1428c99bdf8509a69f
Kamil Pogorzelski <pogorzelski.kamil@gmail.com>
2023-12-28 19:34:09 +0100

    Optimize solution for 2023 day 3

--- solutions/2023/3/3.js
+++ solutions/2023/3/3.js
@@ -1,114 +1,57 @@
 import {
+  eq,
   filter,
+  getAllNeighbors,
+  getPointValue,
   map,
   multiply,
   parseGrid,
   pick,
   pipe,
-  reduce,
+  range,
+  spreadGrid,
   sum,
   toInt,
-  translate,
 } from '../../../utils.js';
 
-const adjacentDirections = [
-  [-1, -1],
-  [-1, 0],
-  [-1, 1],
-  [0, 1],
-  [1, 1],
-  [1, 0],
-  [1, -1],
-  [0, -1],
-];
+const parseInput = pipe(parseGrid, (grid) => {
+  const numbers = grid.flatMap((row, x) =>
+    Array.from(row.join('').matchAll(/\d+/dg)).map((match) => [
+      toInt(match[0]),
+      // TODO change to apply(leftExclusiveRange)
+      range(match.indices[0][0], match.indices[0][1] - 1)
+        .map((y) => [x, y])
+        .flatMap(getAllNeighbors),
+    ])
+  );
+
+  return [grid, numbers];
+});
 
 const first = pipe(
-  parseGrid,
-  (grid) => {
-    const numbers = [];
-    for (let x = 0; x < grid.length; x++) {
-      const row = grid[x];
-      for (let y = 0; y < row.length; y++) {
-        const col = row[y];
-        if (!Number.isNaN(toInt(col))) {
-          const number = [],
-            positions = [];
-          while (!Number.isNaN(toInt(col))) {
-            number.push(col);
-            positions.push([x, y]);
-            y++;
-          }
-          numbers.push([
-            toInt(number.join('')),
-            positions.flatMap((pos) =>
-              adjacentDirections.map((adj) => translate(pos, adj))
-            ),
-          ]);
-        }
-      }
-    }
-
-    return [grid, numbers];
-  },
+  parseInput,
   ([grid, numbers]) =>
     numbers.filter(([number, positions]) =>
-      positions.some(
-        ([x, y]) =>
-          grid[x]?.[y] !== undefined &&
-          grid[x]?.[y] !== '.' &&
-          Number.isNaN(toInt(grid[x]?.[y]))
-      )
+      positions
+        .map(getPointValue(grid))
+        .some(
+          (val) => val !== undefined && val !== '.' && Number.isNaN(toInt(val))
+        )
     ),
   map(pick(0)),
   sum
 );
 
-const findStars = (arr) =>
-  reduce([], (acc, row, x) => {
-    reduce(acc, (innerAcc, col, y) => {
-      if (col === '*') acc.push([x, y]);
-    })(row);
-
-    return acc;
-  })(arr);
-
 const second = pipe(
-  parseGrid,
-  (grid) => {
-    const numbers = [];
-    for (let x = 0; x < grid.length; x++) {
-      const row = grid[x];
-      for (let y = 0; y < row.length; y++) {
-        const col = row[y];
-        if (!Number.isNaN(toInt(col))) {
-          const number = [],
-            positions = [];
-          while (!Number.isNaN(toInt(col))) {
-            number.push(col);
-            positions.push([x, y]);
-            y++;
-          }
-          numbers.push([
-            toInt(number.join('')),
-            positions.flatMap((pos) =>
-              adjacentDirections.map((adj) => translate(pos, adj))
-            ),
-          ]);
-        }
-      }
-    }
-
-    const stars = findStars(grid);
-
-    return stars.map(([x, y]) =>
-      pipe(
-        filter(([number, positions]) =>
-          positions.some((pos) => pos[0] === x && pos[1] === y)
-        ),
-        map(pick(0))
-      )(numbers)
-    );
-  },
+  parseInput,
+  ([grid, numbers]) =>
+    spreadGrid(grid)
+      .filter(([pos, el]) => el === '*')
+      .map(([starPos]) =>
+        numbers
+          .filter(([number, positions]) => positions.some(eq(starPos)))
+          .map(pick(0))
+      ),
   filter((arr) => arr.length === 2),
   map(multiply),
   sum
