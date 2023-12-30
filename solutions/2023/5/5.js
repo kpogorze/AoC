893080d0dcbeefe2406c3023e12117af6c22f751
Kamil Pogorzelski <pogorzelski.kamil@gmail.com>
2023-12-30 01:04:16 +0100

    Clean up solution for 2023 day 5

--- solutions/2023/5/5.js
+++ solutions/2023/5/5.js
@@ -1,109 +1,88 @@
 import {
+  apply,
+  difference,
+  exec,
   filter,
+  intersection,
   map,
   min,
-  pairwise,
   pick,
   pipe,
+  range,
+  reduce,
+  scan,
+  shift,
   split,
+  splitEvery,
+  start,
   toInts,
-} from '../../../utils.js';
+  union,
+} from './utils.js';
 
-const first = pipe(
+const parseInput = pipe(
   split('\n\n'),
   map(split('\n')),
   map(map(toInts)),
-  map(filter((a) => a.length)),
-  ([[seeds], ...mappings]) => {
-    return seeds.map((seed) =>
-      mappings.reduce((id, mapping) => {
-        const targetRange = mapping.find(
-          ([second, first, rangeLen]) => id >= first && id <= first + rangeLen
-        );
+  map(filter((a) => a.length))
+);
 
-        if (!targetRange) return id;
+const prepareSeedPoints = pipe(
+  map((point) => range(point, point + 1)),
+  scan(union)
+);
 
-        return targetRange[0] + (id - targetRange[1]);
-      }, seed)
-    );
-  },
+const prepareMappings = pipe(
+  map(map(([dest, start, len]) => [range(start, start + len), dest - start]))
+);
+
+const mappedSeedRange = (seedRange, mapping) =>
+  exec(
+    mapping,
+    map(([mappingRange, diff]) =>
+      exec(mappingRange, intersection(seedRange), shift(diff))
+    ),
+    scan(union)
+  );
+
+const unmappedSeedRange = (seedRange, mapping) =>
+  difference(seedRange, exec(mapping, map(pick(0)), scan(union)));
+
+const passSeedRangeThroughMappings = (seedRange, mappings) =>
+  reduce(
+    seedRange,
+    (currRange, mapping) =>
+      union(
+        mappedSeedRange(currRange, mapping),
+        unmappedSeedRange(currRange, mapping)
+      ),
+    mappings
+  );
+
+const first = pipe(
+  parseInput,
+  ([[seeds], ...mappings]) => [
+    prepareSeedPoints(seeds),
+    prepareMappings(mappings),
+  ],
+  apply(passSeedRangeThroughMappings),
+  map(start),
   min
 );
 
+const prepareSeedRanges = pipe(
+  splitEvery(2),
+  map(([start, len]) => [start, start + len]),
+  map(apply(range)),
+  scan(union)
+);
+
 const second = pipe(
-  split('\n\n'),
-  map(split('\n')),
-  map(map(toInts)),
-  map(filter((a) => a.length)),
-  ([[seeds], ...mappings]) => {
-    return [
-      seeds
-        .map((id, i) =>
-          i % 2 === 1 ? [seeds[i - 1], seeds[i - 1] + id - 1] : undefined
-        )
-        .filter(Boolean),
-      ...mappings.map((mapping) =>
-        mapping.map(([dest, source, len]) => [
-          [source, source + len - 1],
-          dest - source,
-        ])
-      ),
-    ];
-  },
-  ([seedRanges, ...mappings]) => {
-    return seedRanges.flatMap((seedRange) => {
-      return mappings.reduce(
-        (ranges, mapping) => {
-          return ranges.flatMap(([rangeStart, rangeEnd]) => {
-            const overlapping = mapping
-              .filter(
-                ([[sourceStart, sourceEnd]]) =>
-                  !(sourceEnd < rangeStart || sourceStart > rangeEnd)
-              )
-              .sort(
-                ([firstRange], [secondRange]) => firstRange[0] - secondRange[0]
-              );
-
-            if (overlapping.length === 0) {
-              return [[rangeStart, rangeEnd]];
-            }
-
-            const mappedRange = [];
-
-            overlapping.forEach(([[start, end], diff], i) => {
-              mappedRange.push([
-                Math.max(rangeStart, start) + diff,
-                Math.min(rangeEnd, end) + diff,
-              ]);
-            });
-
-            const [[firstRangeStart, firstRangeEnd]] = overlapping.at(0);
-
-            if (firstRangeStart > rangeStart) {
-              mappedRange.push([rangeStart, firstRangeStart - 1]);
-            }
-
-            const [[lastRangeStart, lastRangeEnd]] = overlapping.at(-1);
-
-            if (lastRangeEnd < rangeEnd) {
-              mappedRange.push([lastRangeEnd + 1, rangeEnd]);
-            }
-
-            pairwise(overlapping).forEach(
-              ([[[firstStart, firstEnd]], [[secondStart, secondEnd]]]) => {
-                if (firstEnd + 1 !== secondStart) {
-                  mappedRange.push([firstEnd + 1, secondStart - 1]);
-                }
-              }
-            );
-
-            return mappedRange;
-          });
-        },
-        [seedRange]
-      );
-    });
-  },
-  map(pick(0)),
+  parseInput,
+  ([[seeds], ...mappings]) => [
+    prepareSeedRanges(seeds),
+    prepareMappings(mappings),
+  ],
+  apply(passSeedRangeThroughMappings),
+  map(start),
   min
 );
