c509af1f5bac6857193094c66e43bd783dc672b3
Kamil Pogorzelski <pogorzelski.kamil@gmail.com>
2023-12-30 01:03:28 +0100

    Add integer range utils

--- utils.js
+++ utils.js
@@ -1,3 +1,12 @@
+/*
+  Consider adding:
+  - some/every equivalent
+  - splitEvery
+  - switch/if-else replacement
+  - orElse
+  - unique
+*/
+
 /*---------------------------------- FUNCTIONS ----------------------------------*/
 export const pipe = (...functions) =>
   functions.reduce(
@@ -5,9 +14,19 @@ export const pipe = (...functions) =>
     (arg) => arg
   );
 
+export const exec = (arg, ...functions) =>
+  functions.reduce((result, func) => func(result), arg);
+
 export const curry = (func) =>
   function curried(...args) {
     return args.length < func.length
+      ? curried.bind(null, ...args)
+      : func.apply(null, args);
+  };
+
+export const curryN = (arity, func) =>
+  function curried(...args) {
+    return args.length < arity
       ? curried.bind(null, ...args)
       : func.apply(null, args);
   };
@@ -24,6 +43,41 @@ export const negate =
   (...args) =>
     !func(...args);
 
+/*---------------------------------- OBJECT ----------------------------------*/
+
+export const pluck = curry((prop, obj) => obj[prop]);
+
+export const log = (arg) => {
+  console.log(arg);
+  return arg;
+};
+
+export const copy = (arg) => structuredClone(arg);
+
+export const mapObject = curry((mapFn, obj) =>
+  Object.fromEntries(Object.entries(obj).map(mapFn))
+);
+
+export const eq = curry((a, b) => {
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return a.length === b.length && a.every((el, i) => eq(el, b[i]));
+  }
+
+  if (
+    a != null &&
+    typeof a === 'object' &&
+    b != null &&
+    typeof b === 'object'
+  ) {
+    if (a[Symbol.iterator] && b[Symbol.iterator]) {
+      return eq([...a], [...b]);
+    }
+    return eq(Object.entries(a), Object.entries(b));
+  }
+
+  return a === b;
+});
+
 /*---------------------------------- COLLECTIONS ----------------------------------*/
 
 export const join = curry((char, arr) => arr.join(char));
@@ -32,6 +86,10 @@ export const split = curry((char, str) => str.split(char));
 
 export const splitByLine = split('\n');
 
+export const splitEvery = curry((n, arr) =>
+  arr.length <= n ? [arr] : [arr.slice(0, n), ...splitEvery(n, arr.slice(n))]
+);
+
 export const sort = curry((compFunc, array) => array.toSorted(compFunc));
 
 export const asc = (a, b) => a - b;
@@ -39,6 +97,10 @@ export const asc = (a, b) => a - b;
 export const desc = (a, b) => b - a;
 
 export const map = curry((func, arr) => arr.map(func));
+
+export const flatten = (arr) => arr.flat();
+
+export const flatMap = curry((func, arr) => arr.flatMap(func));
 
 export const filter = curry((func, arr) => arr.filter(func));
 
@@ -51,6 +113,10 @@ export const scan = curry((func, arr) => arr.slice(1).reduce(func, arr.at(0)));
 export const take = curry((index, arr) => arr.slice(0, index));
 
 export const pick = curry((index, arr) => arr.at(index));
+
+export const start = pick(0);
+
+export const end = pick(-1);
 
 export const count = reduce({}, (acc, curr) => {
   if (acc[curr]) {
@@ -100,43 +166,102 @@ export const gcd = (a, b) => {
 
 export const lcm = (a, b) => (a * b) / gcd(a, b);
 
-export const sequence = (from, to) =>
-  Array.from({ length: to - from + 1 }, (_, i) => i + from);
+export const cartesian = (a, b) => a.flatMap((ai) => b.map((bi) => [ai, bi]));
 
-/*---------------------------------- OBJECT ----------------------------------*/
+/*---------------------------------- RANGES ----------------------------------*/
 
-export const pluck = curry((prop, obj) => obj[prop]);
-
-export const log = (arg) => {
-  console.log(arg);
-  return arg;
-};
-
-export const copy = (arg) => structuredClone(arg);
-
-export const mapObject = curry((mapFn, obj) =>
-  Object.fromEntries(Object.entries(obj).map(mapFn))
+export const sequence = curry((from, to) =>
+  Array.from({ length: to - from + 1 }, (_, i) => i + from)
 );
 
-export const eq = curry((a, b) => {
-  if (Array.isArray(a) && Array.isArray(b)) {
-    return a.length === b.length && a.every((el, i) => eq(el, b[i]));
+export const range = curry((from, to) => [[from, to]]);
+
+const isOverlapping = curry((a, b) => end(a) > start(b) && end(b) > start(a));
+
+const isAdjacent = curry((a, b) => end(a) === start(b) || end(b) === start(a));
+
+const simpleUnion = curry((a, b) => {
+  if (isOverlapping(a, b) || isAdjacent(a, b)) {
+    return [[Math.min(start(a), start(b)), Math.max(end(a), end(b))]];
   }
 
-  if (
-    a != null &&
-    typeof a === 'object' &&
-    b != null &&
-    typeof b === 'object'
-  ) {
-    if (a[Symbol.iterator] && b[Symbol.iterator]) {
-      return eq([...a], [...b]);
-    }
-    return eq(Object.entries(a), Object.entries(b));
-  }
-
-  return a === b;
+  return [a, b];
 });
+
+const simpleIntersection = (a, b) => {
+  if (!isOverlapping(a, b)) {
+    return [];
+  }
+
+  return [[Math.max(start(a), start(b)), Math.min(end(a), end(b))]];
+};
+
+const simpleDiff = (a, b) => {
+  if (!isOverlapping(a, b)) {
+    return [a];
+  }
+
+  return [
+    [start(a), start(b)],
+    [end(b), end(a)],
+  ].filter(([from, to]) => from < to);
+};
+
+const consolidate = pipe(
+  sort((a, b) => start(a) - start(b)),
+  (rang) =>
+    rang.length
+      ? rang
+          .slice(1)
+          .reduce(
+            (newRange, currPart) => [
+              ...newRange.slice(0, -1),
+              ...simpleUnion(end(newRange), currPart),
+            ],
+            [rang[0]]
+          )
+      : rang
+);
+
+export const union = curry((rangeA, rangeB) =>
+  consolidate([...rangeA, ...rangeB])
+);
+
+export const intersection = curry((rangeA, rangeB) =>
+  exec(
+    [rangeA, rangeB],
+    apply(cartesian),
+    flatMap(apply(simpleIntersection)),
+    consolidate
+  )
+);
+
+export const difference = curry((rangeA, rangeB) =>
+  exec(
+    rangeA,
+    map((a) => [[a], rangeB.filter(isOverlapping(a))]),
+    flatMap(([subRange, toDiff]) =>
+      reduce(
+        subRange,
+        (subR, diff) => subR.flatMap((r) => simpleDiff(r, diff)),
+        toDiff
+      )
+    ),
+    consolidate
+  )
+);
+
+export const symmetricDifference = curry((rangeA, rangeB) =>
+  union(difference(rangeA, rangeB), difference(rangeB, rangeA))
+);
+
+export const enumerate = pipe(
+  map(([start, end]) => [start, end - 1]),
+  map(apply(sequence)),
+  flatten
+);
+
+export const shift = curry((n, rang) => rang.map(map((el) => el + n)));
 
 /*---------------------------------- GRID ----------------------------------*/
 
@@ -148,8 +273,6 @@ export const spreadGrid = (grid) =>
 export const translate = curry((point, delta) =>
   point.map((val, i) => val + delta[i])
 );
-
-export const cartesian = (a, b) => a.flatMap((ai) => b.map((bi) => [ai, bi]));
 
 export const transpose = (grid) =>
   grid[0].map((_, col) => grid.map((row) => row[col]));
