b22790bdab3f97b704455d3c408cba8f6b2b6ff8
Kamil Pogorzelski <pogorzelski.kamil@gmail.com>
2024-12-05 17:16:47 +0100

    Commit solutions after copying to solutions dir

--- copy_solution.sh
+++ copy_solution.sh
@@ -5,3 +5,6 @@ target_directory=./solutions/$1/$2
 mkdir -p $target_directory
 cp ./solution.js $target_directory/solution.js
 cp ./input.txt $target_directory/input.txt
+
+git add $target_directory
+git commit -m "$1 day $2 solutions"
